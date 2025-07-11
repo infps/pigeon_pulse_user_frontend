25968b06f9fe4fb82f400fb04286499e25674a05
Tushar Daiya <tushardaiya@outlook.com>
2025-07-11 14:59:45 +0530

    feat: Add session management and loading state to profile layout for improved user experience

--- src/app/(main)/profile/layout.tsx
+++ src/app/(main)/profile/layout.tsx
@@ -3,8 +3,36 @@
 import ProfileSidebar from "@/components/ProfileSidebar";
 import { RacesCarousel } from "@/components/RacesCarousel";
 import { listRaces } from "@/lib/api/race";
+import { useSession } from "@/lib/auth-client";
+import useUserStore from "@/store/store";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { data, error, isPending } = useSession();
+  const router = useRouter();
+  const { setUserData } = useUserStore.getState();
+  useEffect(() => {
+    if (!isPending && data === null) {
+      router.push("/login");
+      return;
+    }
+    if (data) {
+      setUserData(data.session, data.user);
+    }
+  }, [data, isPending, setUserData, router]);
+
+  if (isPending || !data) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-blue-600"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    router.push("/login");
+  }
   return (
     <>
       <div className="w-full">
